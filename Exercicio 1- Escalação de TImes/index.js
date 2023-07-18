94df08a3b52c0a49c63f338db668a89abab67943
matheus <matheudandrade@outlook.com>
2023-07-18 11:20:21 -0300

    first commit

---
+++ Exercicio 1- Escalação de TImes/index.js
@@ -0,0 +1,27 @@
+function addPlayer() {
+  const position = document.getElementById("position").value;
+  const name = document.getElementById("name").value;
+  const number = document.getElementById("number").value;
+
+  const confirmation = confirm("Escalar " + name + "como " + position + "?");
+
+  if (confirmation) {
+    const teamList = document.getElementById("teamList");
+    const playerItem = document.createElement("li");
+    playerItem.id = "player :" + number;
+    playerItem.innerText = position + ": " + name + "( " + number + " ) ";
+    teamList.appendChild(playerItem);
+  }
+}
+
+function removePlayer() {
+  const number = document.getElementById("numberToRemove").value;
+  const playerToRemove = document.getElementById("player :" + number);
+
+  const confirmation = confirm("Deseja remover o " + playerToRemove + "?");
+
+  if (confirmation) {
+    playerToRemove.remove();
+    document.getElementById("numberToRemove").value = "";
+  }
+}
